75f4cfa3b2f6cb27d281bee7aaae75c7d1c0805f
Joel Chang <jcchangg3@gmail.com>
2022-05-07 19:41:29 +0800

    Added option for languages (EN and ES), zh-tw coming soon

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 from myhelpers import generate_list, get_unchanged, login, get_grades, dir_path
 import argparse
+import json
 
-parser = argparse.ArgumentParser(
-    description="Why your NDHU E-Learning password should be different from your usual password.")
-parser.add_argument('--list', type=dir_path,
-                    help="Path to list of students (if you happen to know those who haven't changed their passwords).")
-parser.add_argument('--generate-list', action='store_true',
-                    help="Too lazy to give a list? Use this option. (W.I.P.)")
-parser.add_argument('--skip-try', action='store_true',
-                    help='When you just know that your input is right or that no one changes their passwords.')
-parser.add_argument('--get-grades', action='store_true',
-                    help="If you also need to know grades for some reason, proceed with caution.")
-parser.add_argument('--browser', type=str, default='chrome',
-                    help="Your favorite browser, as long as it's either chrome or firefox.")
+
+helpdoc = json.load(open("helpdoc.json",))
+parser = argparse.ArgumentParser(description=helpdoc["desc"])
+parser.add_argument('--list', type=dir_path, help=helpdoc["list"])
+parser.add_argument('--generate-list', action='store_true', help=helpdoc["genlist"])
+parser.add_argument('--skip-try', action='store_true', help=helpdoc["skiptry"])
+parser.add_argument('--get-grades', action='store_true', help=helpdoc["getgrades"])
+parser.add_argument('--browser', type=str, default='chrome', help=helpdoc["browser"])
+parser.add_argument('--lang', type=str, default='en',help=helpdoc["lang"])
 args = parser.parse_args()
 
 print(args)
+
+if args.lang:
+    open("helpdoc.json", "w").write(open(f"helpdoc_{args.lang}.json").read())
+    helpdoc = json.load(open("helpdoc.json",))
+    print(helpdoc['lang_ok'])
+    exit()
 
 candidates = []
 if args.list:
@@ -36,11 +40,10 @@ if args.skip_try:
     print('--skip-try argument given. Will skip trying to log in.')
     unchanged = candidates
 else:
-    print('Will try to login with default passwords.')
-    unchanged = get_unchanged(candidates, args.browser)
-
-if args.get_grades:
-    for user in unchanged:
-        driver = login(user, user, args.browser)
-        print(f"Getting user {user} grades now.")
-        get_grades(driver, user, user)
+    if args.get_grades:
+        print('Will try to login with default passwords.')
+        unchanged = get_unchanged(candidates, args.browser)
+        for user in unchanged:
+            driver = login(user, user, args.browser)
+            print(f"Getting user {user} grades now.")
+            get_grades(driver, user, user)
